20c3b1056be510790ecdafff3ca35f3a15866a2d
TalbiaChoudhury <eirfatahmedchoudhury@gmail.com>
2025-07-22 12:51:50 +0100

    Add files via upload

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,23 +6,45 @@ import Link from 'next/link';
 import styles from './Navbar.module.css';
 import Image from 'next/image';
 import { useState } from 'react'; // Import useState
-import logo from '../../../public/assets/images/logo.png'; // Import the logo image
+import logoWithText from '../../../public/assets/images/logo with text.png'; // Import the logo image
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
+  // Function to close the menu, useful for mobile view
+  const closeMenu = () => setMenuOpen(false);
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.container}>
-        <Link href="/" className={styles.logo}>
-          <Image
-            src={logo}
-            alt="Logo"
-            width={50}
-            height={50}
-          />
-          Icon
-        </Link>
+        {/* Part 1: Logo */}
+        <div className={styles.navSection}>
+          <Link href="/" className={styles.logo}>
+            <Image
+              src={logoWithText}
+              alt="Logo"
+              width={100}
+              height={50}
+            />
+          </Link>
+        </div>
+
+        {/* Part 2: Centered links for desktop */}
+        <div className={`${styles.navSection} ${styles.navCenter}`}>
+          <ul className={styles.desktopLinks}>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/">Trainers</Link></li>
+            <li><Link href="/">Vision</Link></li>
+            <li><Link href="/">About us</Link></li>
+          </ul>
+        </div>
+        
+        {/* Part 3: Contact button for desktop */}
+        <div className={`${styles.navSection} ${styles.navRight}`}>
+            <Link href="/" className={styles.button}>Sign Up</Link>
+        </div>
+
+        {/* Hamburger for mobile */}
         <div 
           className={styles.hamburger} 
           onClick={() => setMenuOpen(!menuOpen)}
@@ -31,16 +53,24 @@ const Navbar = () => {
           <div className={styles.bar}></div>
           <div className={styles.bar}></div>
         </div>
+
+        {/* This list now serves as the mobile menu overlay */}
         <ul className={`${styles.navLinks} ${menuOpen ? styles.active : ''}`}>
           <li>
-            <Link href="/" onClick={() => setMenuOpen(false)}>Home</Link>
+            <Link href="/" onClick={closeMenu}>Home</Link>
           </li>
           <li>
-            <Link href="/" onClick={() => setMenuOpen(false)}>About Us</Link>
+            <Link href="/" onClick={closeMenu}>About</Link>
           </li>
           <li>
-            {/* Apply the button class to the Contact Us link */}
-            <Link href="/" className={styles.button} onClick={() => setMenuOpen(false)}>Contact Us</Link>
+            <Link href="/" onClick={closeMenu}>Features</Link>
+          </li>
+          <li>
+            <Link href="/" onClick={closeMenu}>Pricing</Link>
+          </li>
+          <li>
+            {/* The button is a regular list item in the mobile menu */}
+            <Link href="/" className={styles.button} onClick={closeMenu}>Sign Up</Link>
           </li>
         </ul>
       </div>
