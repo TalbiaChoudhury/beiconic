8793de882ee71c96d393a422f9196d01a9e37614
TalbiaChoudhury <eirfatahmedchoudhury@gmail.com>
2025-07-28 13:45:10 +0100

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,9 @@
 "use client";
 
 import Hero from "../components/Hero/Hero";
-import SideBySide from "../components/SideBySide/SideBySide";
 import FadeIn from "../components/FadeIn/FadeIn";
 import FadeInHero from "../components/FadeIn/FadeInHero";
 import CarouselSlides from '../components/CarouselSlides/CarouselSlides';
-import Pricing from "@/components/Pricing/Pricing";
-import gym1 from "../../public/assets/images/gym1.jpg";
-import gym2 from "../../public/assets/images/gym2.jpg";
 
 export default function Home() {
   const handleDemoClick = () => {
@@ -21,37 +17,7 @@ export default function Home() {
       </FadeInHero>
 
       <FadeIn>
-        <SideBySide
-          imagePosition="right"
-          header="Finally, a Plan That Actually Listens"
-          text="Your AI coach isn't just a list of exercises. It's a dynamic conversation. Tell it how you're feeling, what equipment you have, or if you're short on time. It will adapt your plan on the spot, just like a real personal trainer would."
-          buttonLabel="Learn More"
-          onButtonClick={handleDemoClick}
-          imageSrc={gym1}
-          imageAlt="Placeholder image"
-        />
-      </FadeIn>
-
-      <FadeIn>
-        <SideBySide
-          imagePosition="left"
-          header="Find the Perfect Coach for Your Style"
-          bulletPoints={[
-            "Testing bullet point number 1",
-            "Testing an incredibly long bullet point that should wrap to the next line if it exceeds the width of the container",
-          ]}
-          listType="number"
-          imageSrc={gym2}
-          imageAlt="Placeholder image"
-        />
-      </FadeIn>
-
-      <FadeIn>
         <CarouselSlides />
-      </FadeIn>
-
-      <FadeIn>
-        <Pricing />
       </FadeIn>
     </>
   );
