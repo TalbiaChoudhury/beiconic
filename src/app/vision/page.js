cd1dc0efe9106b828b2b48a30e074a11516489c9
TalbiaChoudhury <eirfatahmedchoudhury@gmail.com>
2025-08-04 17:42:01 +0100

    Add files via upload

--- src/app/vision/page.js
+++ src/app/vision/page.js
@@ -1,13 +1,14 @@
 'use client';
 
 import React from 'react';
+import VisionComponent from '@/components/Vision/VisionComponent/VisionComponent';
 
-const Trainers = () => {
+const Vision = () => {
   return (
     <div>
-
+      <VisionComponent />
     </div>
   );
 };
 
-export default Trainers;
+export default Vision;
