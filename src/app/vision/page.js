0c3103a0fd263257a061a53eb4dfa551eddaf473
MishChoudhury <Miskatchoudhury@gmail.com>
2025-08-06 23:55:06 +0100

    Add files via upload

--- src/app/vision/page.js
+++ src/app/vision/page.js
@@ -1,8 +1,22 @@
-// app/vision/page.js
-import VisionCarousel from '@/components/Vision/VisionCarousel'; // The path might be slightly different
+"use client";
+
+import VisionCarousel from '@/components/Vision/VisionCarousel';
+import { useEffect } from 'react'; // Make sure useEffect is imported
 
 export default function VisionPage() {
+  
+  // Add this entire useEffect block
+  useEffect(() => {
+  document.body.classList.add('full-height-page');
+  document.documentElement.classList.add('full-height-page'); // Add this line
+
+  return () => {
+    document.body.classList.remove('full-height-page');
+    document.documentElement.classList.remove('full-height-page'); // Add this line
+  };
+}, []);
+
   return (
-    <VisionCarousel />
+      <VisionCarousel />
   );
 }
